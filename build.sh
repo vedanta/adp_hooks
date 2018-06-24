2cc6df183792061bd111469bbe72a6c67f55efeb
Vedanta Barooah <barooah.vedanta@gmail.com>
2018-06-24 08:08:32 -0700

    finalize script

--- build.sh
+++ build.sh
@@ -2,14 +2,15 @@
 
 set -euo pipefail
 
-name="wordpress"
+name="redis"
 type="salt"
 salt_config="https://raw.githubusercontent.com/vedanta/sample_wp/master/conf/minion"
 salt_config_dir="/srv/salt/conf"
 
 entrypoint_salt(){
 	echo "Installing saltstack"
-   	sudo yum -y install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
+   	rpm -qa | grep -qw salt-repo-latest || \
+	sudo yum -y install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
 	sudo yum -y install epel-release
    	sudo yum clean expire-cache 
 	sudo yum -y install salt-minion
