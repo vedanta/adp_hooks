3a5d4ead8a87d9faee96e0433a66e6f4fc04937a
Vedanta Barooah <barooah.vedanta@gmail.com>
2018-06-25 11:05:26 -0700

    removed sudo

--- build.sh
+++ build.sh
@@ -10,16 +10,16 @@ salt_config_dir="/srv/salt/conf"
 entrypoint_salt(){
 	echo "Installing saltstack"
    	rpm -qa | grep -qw salt-repo-latest || \
-	sudo yum -y install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
-	sudo yum -y install epel-release
-   	sudo yum clean expire-cache 
-	sudo yum -y install salt-minion
-	sudo yum -y install GitPython
-	sudo yum -y install wget
-    	sudo mkdir -p $salt_config_dir
-    	wget -c --directory-prefix=$salt_config_dir $salt_config 
-    	# exec the base 
-   	sudo salt-call --config-dir=/srv/salt/conf --local state.apply
+	yum -y install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
+	yum -y install epel-release
+   	yum clean expire-cache 
+	yum -y install salt-minion
+	yum -y install GitPython
+	yum -y install wget
+   	mkdir -p $salt_config_dir
+   	wget -c --directory-prefix=$salt_config_dir $salt_config 
+   	# exec the base 
+   	salt-call --config-dir=/srv/salt/conf --local state.apply
 }
 case $type in
     "salt")
